d601b0dc814ea63960b2ae81e9972391a9b88f14
OliviaTheVampire <sindrefagerheim53@gmail.com>
2019-07-25 23:50:10 +0200

    removed MathUtil + made some changes in HGBiomes

--- src/main/java/team/abnormals/hidden_gems/init/HGBiomes.java
+++ src/main/java/team/abnormals/hidden_gems/init/HGBiomes.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.feature.Feature;
 import team.abnormals.hidden_gems.HiddenGems;
 import team.abnormals.hidden_gems.world.biome.BlossomForest;
 
@@ -13,19 +14,26 @@ public class HGBiomes {
 
     public static Biome CANYON;
     public static Biome CANYON_CLIFFS;
-    public static final Biome BLOSSOM_FOREST;
+    public static final Biome BLOSSOM_FOREST = new BlossomForest();
 
     static {
-        BLOSSOM_FOREST = register("blossom_forest", new BlossomForest());
+        register("blossom_forest", BLOSSOM_FOREST);
+        forEveryFeature(feature -> HiddenGems.LOGGER.info("Found feature: " + Registry.FEATURE.getId(feature).toString()));
+        forEveryBiome(biome -> HiddenGems.LOGGER.info("Found biome: " + Registry.BIOME.getId(biome).toString()));
     }
 
-    public static <T extends Biome> T register(String name, T biome) {
-        return Registry.register(Registry.BIOME, new Identifier(HiddenGems.MOD_ID, name), biome);
+    public static void register(String name, Biome biome) {
+        Registry.register(Registry.BIOME, new Identifier(HiddenGems.MOD_ID, name), biome);
     }
 
     private static void forEveryBiome(Consumer<Biome> biomes) {
         Registry.BIOME.forEach(biomes);
         RegistryEntryAddedCallback.event(Registry.BIOME).register((rawId, id, biome) -> biomes.accept(biome));
+    }
+
+    private static void forEveryFeature(Consumer<Feature> features) {
+        Registry.FEATURE.forEach(features);
+        RegistryEntryAddedCallback.event(Registry.FEATURE).register((rawId, id, feature) -> features.accept(feature));
     }
 
 }
--- src/main/java/team/abnormals/hidden_gems/utils/MathUtil.java
+++
@@ -1,9 +0,0 @@
-package team.abnormals.hidden_gems.utils;
-
-public class MathUtil {
-
-	public interface Equation {
-		public double compute(double theta);
-	}
-
-}
